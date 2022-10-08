40b56bb476f0b78a101c66a4749047b421d2dfae
jupiter67s <jupiter-67@vivaldi.net>
2022-10-08 21:12:33 +0300

    добавил настройки

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from hypercorn.asyncio import serve, Config
 from fastapi import FastAPI
 import uvloop
 
+from settings import Settings
 
+
+settings = Settings()
 app = FastAPI()
 
 
 if __name__ == '__main__':
     config = Config()
-    config.bind = '0.0.0.0:8000'
+    config.bind = f'{settings.host}: {settings.port}'
     config.accesslog = '-'
     config.errorlog = '-'
     uvloop.install()
